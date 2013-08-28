7f688cb378e754896382cdaf9c7c5d77b3a5c60c
paulocheque <paulocheque@gmail.com>
2013-08-28 09:53:23 -0300

    Fixed imports and package config after repository migration

--- gat_games/__init__.py
+++ gat_games/__init__.py
@@ -1,13 +1,2 @@
 # coding: utf-8
-import sys
-
 VERSION = '0.0.2'
-
-if sys.version_info[0] == 2:
-    from gat_games.games.truco import *
-    from gat_games.games.truco_clean_deck import *
-else:
-    from .games.truco import *
-    from .games.truco_clean_deck import *
-
-SUPPORTED_GAMES = [Truco, TrucoCleanDeck]
